eabd15ca19f4bad61e2f8847ee33080379c5fcfa
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-05-25 12:11:48 +0530

    BO Factory done

--- JavaEE_Servlets/src/bo/BOFactory.java
+++ JavaEE_Servlets/src/bo/BOFactory.java
@@ -1,4 +1,39 @@
 package bo;
 
+import bo.custom.impl.CustomerBOImpl;
+import bo.custom.impl.ItemBOImpl;
+import bo.custom.impl.OrderBOImpl;
+import bo.custom.impl.OrderDetailsBOImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory(){
+
+    }
+
+    public static BOFactory getBoFactory(){
+        if(boFactory == null){
+            boFactory= new BOFactory();
+        }
+        return boFactory;
+    }
+
+    public SuperBO getBO(BoTypes types){
+        switch (types){
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case ITEM:
+                return new ItemBOImpl();
+            case ORDER:
+                return new OrderBOImpl();
+            case ORDER_DETAILS:
+                return new OrderDetailsBOImpl();
+            default:
+                return null;
+        }
+    }
+    public enum  BoTypes{
+        CUSTOMER,ITEM,ORDER,ORDER_DETAILS
+    }
 }
