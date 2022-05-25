63c2327ae96875e939a0c1e3c36cf6cee5837f87
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-05-25 12:01:14 +0530

    dao factory added

--- JavaEE_Servlets/src/dao/DAOFactory.java
+++ JavaEE_Servlets/src/dao/DAOFactory.java
@@ -1,4 +1,36 @@
 package dao;
 
+import dao.custom.impl.CustomerDAOImpl;
+import dao.custom.impl.ItemDAOImpl;
+import dao.custom.impl.OrderDAOImpl;
+import dao.custom.impl.OrderDetailsDAOImpl;
+
 public class DAOFactory {
+        private static DAOFactory daoFactory;
+
+        public static DAOFactory getDAOFactory() {
+            if (daoFactory == null) {
+                daoFactory = new DAOFactory();
+            }
+            return daoFactory;
+        }
+
+        public SuperDAO getDAO(DAOTypes type){
+            switch (type){
+                case CUSTOMER:
+                    return new CustomerDAOImpl();
+                case ITEM :
+                    return new ItemDAOImpl();
+                case ORDER:
+                    return new OrderDAOImpl();
+                case ORDER_DETAILS:
+                    return new OrderDetailsDAOImpl();
+                default:
+                    return null;
+            }
+        }
+
+        public enum DAOTypes {
+            CUSTOMER,ITEM,ORDER,ORDER_DETAILS
+        }
 }
